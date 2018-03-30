ed4a12d5085048c6cf23566b70a1a8852dd16432
Karol Jezierski <kajezierski@wp.pl>
2018-03-30 19:48:57 +0000

    Add voting and ordering by number of votes

--- Gemfile
+++ Gemfile
@@ -30,6 +30,14 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+
+gem 'devise'
+gem 'bootstrap-sass', '~> 3.3.7'
+gem 'jquery-rails'
+gem 'acts_as_votable'
+
+
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  devise_for :users
+  root 'home#index'
 
-  resources :movies
+  resources :movies do
+    member do
+      put "like", to: 'movies#upvote'
+      put "unlike", to: 'movies#downvote'
+    end
+  end
+  
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
